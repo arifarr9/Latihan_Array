0c6b83b742962181c7deaa1989a5d47c8b5f9db8
kadalgurun2223 <mochfadilahrivaldi@gmail.com>
2024-01-09 12:58:36 +0700

    first commit

---
+++ lat3.php
@@ -0,0 +1,8 @@
+<?php 
+//contoh array
+$cars = array("Ferrary", "lamborghini avendator", "Avanza");
+
+//menampilkan array
+print_r($cars);
+echo"<br>";
+?>
---
+++ lat5.php
@@ -0,0 +1,16 @@
+<?php
+    // $nomor=array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+    // foreach($nomor as $angka){
+    //     if ($angka % 2 == 1){
+    //         echo $angka;
+    //         echo "<br>";
+    // }
+    // }
+
+    for ($x=1; $x <= 20; $x++){ 
+      if ( $x % 2 ==1 ){
+        echo " " .$x; 
+        echo "<br>";
+      }
+    }
+?>
